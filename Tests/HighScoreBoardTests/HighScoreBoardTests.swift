feab1f1646a3f623b60f527be12b3eac301b66cc
Gianluca Orpello <g.orpello@gmail.com>
2024-07-31 15:16:19 +0200

    Remove skip test

--- Tests/HighScoreBoardTests/HighScoreBoardTests.swift
+++ Tests/HighScoreBoardTests/HighScoreBoardTests.swift
@@ -3,14 +3,13 @@ import XCTest
 @testable import HighScoreBoard
 
 final class HighScoreBoardTests: XCTestCase {
-  let runAll = Bool(ProcessInfo.processInfo.environment["RUNALL", default: "false"]) ?? false
 
   func testEmptyScores() {
     XCTAssertEqual(newScoreBoard(), [String: Int]())
   }
 
   func testAddPlayerExplicit() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     let score = 1337
     addPlayer(&scoreboard, "Jesse Johnson", score)
@@ -26,7 +25,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testAddPlayerDefault() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson")
     if let jjScore = scoreboard["Jesse Johnson"] {
@@ -41,7 +40,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testRemovePlayer() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson", 1337)
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
@@ -54,7 +53,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testRemoveNonexistentPlayer() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson", 1337)
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
@@ -68,7 +67,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testResetScore() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson", 1337)
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
@@ -82,7 +81,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testUpdateScore() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson", 1337)
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
@@ -97,7 +96,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testOrderByPlayers() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson", 1337)
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
@@ -113,7 +112,7 @@ final class HighScoreBoardTests: XCTestCase {
   }
 
   func testOrderByScores() throws {
-    try XCTSkipIf(true && !runAll)  // change true to false to run this test
+    
     var scoreboard = [String: Int]()
     addPlayer(&scoreboard, "Jesse Johnson", 1337)
     addPlayer(&scoreboard, "Amil PAstorius", 99373)
