755f5bc48c527be4eaa0233d6e1056c39a295990
Gianluca Orpello <g.orpello@gmail.com>
2024-10-21 16:25:38 +0200

    Switch to Swift Testing

--- Tests/HighScoreBoardTests/HighScoreBoardTests.swift
+++ Tests/HighScoreBoardTests/HighScoreBoardTests.swift
@@ -1,140 +1,88 @@
-import XCTest
+import Testing
 
-@testable import HighScoreBoard
-
-final class HighScoreBoardTests: XCTestCase {
-
-  func testEmptyScores() {
-    XCTAssertEqual(newScoreBoard(), [String: Int]())
-  }
-
-  func testAddPlayerExplicit() throws {
+struct HighScoreBoardTests {
     
-    var scoreboard = [String: Int]()
-    let score = 1337
-    addPlayer(&scoreboard, "Jesse Johnson", score)
-    if let jjScore = scoreboard["Jesse Johnson"] {
-      XCTAssertEqual(
-        jjScore,
-        score,
-        "Called 'addPlayer(&scoreboard, \"Jesse Johnson\", 1337)'\nscoreboard[\"Jesse Johnson\"] expected to be \(score), got \(jjScore) instead"
-      )
-    } else {
-      XCTFail("addPlayer failed to add player to dictionary")
+    @Test func testEmptyScoresTest() async throws {
+        #expect(newScoreBoard() == [:])
     }
-  }
-
-  func testAddPlayerDefault() throws {
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson")
-    if let jjScore = scoreboard["Jesse Johnson"] {
-      XCTAssertEqual(
-        jjScore,
-        0,
-        "Called 'addPlayer(&scoreboard, \"Jesse Johnson\"'\nscoreboard[\"Jesse Johnson\"] expected to be \(0), got \(jjScore) instead"
-      )
-    } else {
-      XCTFail("addPlayer failed to add player to dictionary")
+    @Test func testAddPlayerExplicitTest() async throws {
+        var scoreboard = [String: Int]()
+        let score = 1337
+        addPlayer(&scoreboard, "Jesse Johnson", score)
+        let jjScore = try #require(scoreboard["Jesse Johnson"])
+        #expect(jjScore == score)
     }
-  }
-
-  func testRemovePlayer() throws {
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson", 1337)
-    addPlayer(&scoreboard, "Amil PAstorius", 99373)
-    addPlayer(&scoreboard, "Min-seo Shin")
-    removePlayer(&scoreboard, "Jesse Johnson")
-    XCTAssertNil(
-      scoreboard["Jesse Johnson"],
-      "Removed player \"Jesse Johnson\", \nscoreboard[\"Jesse Johnson\"] expected to be nil, got \(scoreboard["Jesse Johnson"]!) instead"
-    )
-  }
-
-  func testRemoveNonexistentPlayer() throws {
+    @Test func testAddPlayerDefaultTest() async throws {
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson")
+        let jjScore = try #require(scoreboard["Jesse Johnson"])
+        #expect(jjScore == 0)
+    }
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson", 1337)
-    addPlayer(&scoreboard, "Amil PAstorius", 99373)
-    addPlayer(&scoreboard, "Min-seo Shin")
-    removePlayer(&scoreboard, "Bruno Santangelo")
-    XCTAssertEqual(
-      scoreboard,
-      ["Jesse Johnson": 1337, "Amil PAstorius": 99373, "Min-seo Shin": 0],
-      "Removing a non-existent player from the dictionary should leave the dictionary unchanged."
-    )
-  }
-
-  func testResetScore() throws {
+    @Test func testRemovePlayerTest() async throws {
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson", 1337)
+        addPlayer(&scoreboard, "Amil PAstorius", 99373)
+        addPlayer(&scoreboard, "Min-seo Shin")
+        removePlayer(&scoreboard, "Jesse Johnson")
+        #expect(scoreboard["Jesse Johnson"] == nil)
+    }
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson", 1337)
-    addPlayer(&scoreboard, "Amil PAstorius", 99373)
-    addPlayer(&scoreboard, "Min-seo Shin")
-    resetScore(&scoreboard, "Amil PAstorius")
-    XCTAssertEqual(
-      scoreboard,
-      ["Jesse Johnson": 1337, "Amil PAstorius": 0, "Min-seo Shin": 0],
-      "Resetting a player's score should change their score to 0 and leave the rest unchanged."
-    )
-  }
-
-  func testUpdateScore() throws {
+    @Test func testRemoveNonexistentPlayerTest() async throws {
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson", 1337)
+        addPlayer(&scoreboard, "Amil PAstorius", 99373)
+        addPlayer(&scoreboard, "Min-seo Shin")
+        removePlayer(&scoreboard, "Bruno Santangelo")
+        #axpect(scoreboard == ["Jesse Johnson": 1337, "Amil PAstorius": 99373, "Min-seo Shin": 0])
+    }
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson", 1337)
-    addPlayer(&scoreboard, "Amil PAstorius", 99373)
-    addPlayer(&scoreboard, "Min-seo Shin")
-    updateScore(&scoreboard, "Min-seo Shin", 1999)
-    updateScore(&scoreboard, "Jesse Johnson", 1337)
-    XCTAssertEqual(
-      scoreboard,
-      ["Jesse Johnson": 2674, "Amil PAstorius": 99373, "Min-seo Shin": 1999],
-      "Updating a player's score should add the update to their score and leave the rest unchanged."
-    )
-  }
-
-  func testOrderByPlayers() throws {
+    @Test func testResetScoreTest() async throws {
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson", 1337)
+        addPlayer(&scoreboard, "Amil PAstorius", 99373)
+        addPlayer(&scoreboard, "Min-seo Shin")
+        resetScore(&scoreboard, "Amil PAstorius")
+        #expect(scoreboard == ["Jesse Johnson": 1337, "Amil PAstorius": 0, "Min-seo Shin": 0])
+    }
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson", 1337)
-    addPlayer(&scoreboard, "Amil PAstorius", 99373)
-    addPlayer(&scoreboard, "Min-seo Shin")
-    updateScore(&scoreboard, "Min-seo Shin", 1999)
-    updateScore(&scoreboard, "Jesse Johnson", 1337)
-    let expected = [("Amil PAstorius", 99373), ("Jesse Johnson", 2674), ("Min-seo Shin", 1999)]
-    let got = orderByPlayers(scoreboard)
-    XCTAssertTrue(
-      expected.map(\.0) == got.map(\.0) && expected.map(\.1) == got.map(\.1),
-      "Expected: \(expected)\nGot: \(got)\norderByPlayers should return the key/value pairs odered descending by the player's name."
-    )
-  }
-
-  func testOrderByScores() throws {
+    @Test func testUpdateScoreTest() async throws {
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson", 1337)
+        addPlayer(&scoreboard, "Amil PAstorius", 99373)
+        addPlayer(&scoreboard, "Min-seo Shin")
+        updateScore(&scoreboard, "Min-seo Shin", 1999)
+        updateScore(&scoreboard, "Jesse Johnson", 1337)
+        #expect(scoreboard == ["Jesse Johnson": 2674, "Amil PAstorius": 99373, "Min-seo Shin": 1999])
+    }
     
-    var scoreboard = [String: Int]()
-    addPlayer(&scoreboard, "Jesse Johnson", 1337)
-    addPlayer(&scoreboard, "Amil PAstorius", 99373)
-    addPlayer(&scoreboard, "Min-seo Shin")
-    updateScore(&scoreboard, "Min-seo Shin", 1999)
-    let expected = [("Amil PAstorius", 99373), ("Min-seo Shin", 1999), ("Jesse Johnson", 1337)]
-    let got = orderByScores(scoreboard)
-    XCTAssertTrue(
-      expected.map(\.0) == got.map(\.0) && expected.map(\.1) == got.map(\.1),
-      "Expected: \(expected)\nGot: \(got)\norderByPlayers should return the key/value pairs odered ascending by the player's score."
-    )
-  }
-
-  static var allTests = [
-    ("testEmptyScores", testEmptyScores),
-    ("testAddPlayerExplicit", testAddPlayerExplicit),
-    ("testAddPlayerDefault", testAddPlayerDefault),
-    ("testRemovePlayer", testRemovePlayer),
-    ("testRemoveNonexistentPlayer", testRemoveNonexistentPlayer),
-    ("testResetScore", testResetScore),
-    ("testUpdateScore", testUpdateScore),
-    ("testOrderByPlayers", testOrderByPlayers),
-    ("testOrderByScores", testOrderByScores),
-  ]
+    @Test func testOrderByPlayersTest() async throws {
+        
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson", 1337)
+        addPlayer(&scoreboard, "Amil PAstorius", 99373)
+        addPlayer(&scoreboard, "Min-seo Shin")
+        updateScore(&scoreboard, "Min-seo Shin", 1999)
+        updateScore(&scoreboard, "Jesse Johnson", 1337)
+        let expected = [("Amil PAstorius", 99373), ("Jesse Johnson", 2674), ("Min-seo Shin", 1999)]
+        let got = orderByPlayers(scoreboard)
+        XCTAssertTrue(
+            expected.map(\.0) == got.map(\.0) && expected.map(\.1) == got.map(\.1),
+            "Expected: \(expected)\nGot: \(got)\norderByPlayers should return the key/value pairs odered descending by the player's name."
+        )
+    }
+    
+    @Test func testOrderByScoresTest() async throws {
+        var scoreboard = [String: Int]()
+        addPlayer(&scoreboard, "Jesse Johnson", 1337)
+        addPlayer(&scoreboard, "Amil PAstorius", 99373)
+        addPlayer(&scoreboard, "Min-seo Shin")
+        updateScore(&scoreboard, "Min-seo Shin", 1999)
+        let expected = [("Amil PAstorius", 99373), ("Min-seo Shin", 1999), ("Jesse Johnson", 1337)]
+        let got = orderByScores(scoreboard)
+        #expect(expected.map(\.0) == got.map(\.0) && expected.map(\.1) == got.map(\.1) == true)
+    }
+    
 }
